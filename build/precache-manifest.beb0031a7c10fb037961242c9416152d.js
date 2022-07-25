df9a756a3ca35c1b25fd12befa8918afea5dc664
Sangwoo <49279776+sangwoo7957@users.noreply.github.com>
2022-07-25 23:49:53 +0900

    Add files via upload
    cryptocurrency_exchange commit

---
+++ build/precache-manifest.beb0031a7c10fb037961242c9416152d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e4c62a6e703c531b7f2",
+    "url": "/static/css/main.323663b5.chunk.css"
+  },
+  {
+    "revision": "5e4c62a6e703c531b7f2",
+    "url": "/static/js/main.13714d41.chunk.js"
+  },
+  {
+    "revision": "dad9c00322b11f8fd55a",
+    "url": "/static/js/runtime~main.995c7e53.js"
+  },
+  {
+    "revision": "5a3e9c9aeef8aa740b4f",
+    "url": "/static/js/2.d3800601.chunk.js"
+  },
+  {
+    "revision": "e935eb83b194f5ad6a4b",
+    "url": "/static/js/3.9b8b76bf.chunk.js"
+  },
+  {
+    "revision": "dcf5ca986ac8bf4a50c1d8217e185fd8",
+    "url": "/index.html"
+  }
+];
